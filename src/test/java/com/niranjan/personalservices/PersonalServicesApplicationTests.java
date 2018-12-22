da417b517a45b8b9451c5de66d6c4b6b430c4892
Niranjan Maharana <niranjanmaharana95@gmail.com>
2018-12-22 13:00:28 +0530

    initial commit
    Initial commit includes Application File and pom.xml

---
+++ src/test/java/com/niranjan/personalservices/PersonalServicesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.niranjan.personalservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
